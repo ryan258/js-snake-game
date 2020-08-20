b24a891517d57d387d831ff1ee3926fd1a29b509
Ryan Johnson <ryanleejwebdev@gmail.com>
2020-08-20 12:04:05 -0500

    Set up arrow controls and border barriers.

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const score = document.getElementById("score");
 let squares = [];
 let currentSnake = [2, 1, 0];
 let direction = 1;
+const width = 10;
 
 function createGrid() {
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < width * width; i++) {
     const square = document.createElement("div");
     square.classList.add("square");
     grid.appendChild(square);
@@ -19,17 +20,57 @@ createGrid();
 currentSnake.forEach((index) => squares[index].classList.add("snake"));
 
 function move() {
+  if (
+    // if snake hits bottom wall
+    (currentSnake[0] + width >= width * width && direction === width) ||
+    // if snake hits right wall
+    (currentSnake[0] % width === width - 1 && direction === 1) ||
+    // if snake hits left wall
+    (currentSnake[0] % width === 0 && direction === -1) ||
+    // if snake hits top wall
+    (currentSnake[0] - width < 0 && direction === -width) ||
+    // snake can't go into itself
+    squares[currentSnake[0] + direction].classList.contains("snake")
+  ) {
+    return clearInterval(timerId);
+  }
+
   // remove last element from currentSnake array
   const tail = currentSnake.pop();
-  console.log(tail);
-  console.log(currentSnake);
   // remove styling from last element
   squares[tail].classList.remove("snake");
   // add square in direction snake is heading
   currentSnake.unshift(currentSnake[0] + direction);
-  console.log(currentSnake);
   // add styling to see it
   squares[currentSnake[0]].classList.add("snake");
 }
 
 move();
+
+let timerId = setInterval(move, 1000);
+
+// clearInterval(timerId);
+
+// - Keycodes
+// - - 39 / Right Arrow
+// - - 38 / Up Arrow
+// - - 37 / Left Arrow
+// - - 40 / Down Arrow
+
+function control(e) {
+  if (e.keyCode === 39) {
+    console.log("right pressed!");
+    direction = 1;
+  } else if (e.keyCode === 38) {
+    console.log("up pressed!");
+    direction = -width;
+  } else if (e.keyCode === 37) {
+    console.log("left pressed!");
+    direction = -1;
+  } else if (e.keyCode === 40) {
+    console.log("down pressed!");
+    direction = width;
+  }
+}
+
+document.addEventListener("keydown", control);
